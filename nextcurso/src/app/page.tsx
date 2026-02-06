d66c92aba4111bd8e4c412bf429138c9f20b4545
Yanrato <yanferreira985@gmail.com>
2026-02-06 14:28:18 -0300

    testes

--- nextcurso/src/app/page.tsx
+++ nextcurso/src/app/page.tsx
@@ -1,14 +1,9 @@
-import Link from "next/link";
+import React from 'react'
 
-export default function Home() {
+const HomePage = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-            Home Page
-        </div>
-        <Link href="/sobre">Ir para Sobre</Link>
-      </main>
-    </div>
-  );
+    <h1>Olá de dentro do page</h1>
+  )
 }
+
+export default HomePage
