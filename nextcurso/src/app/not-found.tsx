c8b7b5924e68441dcf44d40452f8baeed2a62267
Yanrato <yanferreira985@gmail.com>
2026-02-11 09:32:31 -0300

    pagina de erro

---
+++ nextcurso/src/app/not-found.tsx
@@ -0,0 +1,13 @@
+export default function NoteFoundPage() {
+  return (
+    <div className="min-h-[320px] bg-slate-100 text-slate-900  dark:bg-slate-900 dark:text-slate-100 
+    mb-16 p-8 rounded-xl 
+    flex items-center justify-center
+    text-center">
+      <div>
+      <h1 className="text-7xl/tight mb-4 font-extrabold">404 - Página Não Encontrada</h1>
+      <p>Ops! A página que você está procurando não existe.</p>
+      </div> 
+    </div>
+  );
+}
